7108eb98208d4f29d1d544c1719b304418211fe4
Gendalf_Istari_2016 <palantir2018see@gmail.com>
2024-02-23 22:58:13 +0200

    54949849

---
+++ Install_Administration.ps1
@@ -0,0 +1,12 @@
+﻿$Title="Установка Прав Администратора  (автор Barlog Udun)"
+$EnterPause="Жмите Enter......................"
+$host.UI.RawUI.WindowTitle = $Title
+Write-Host -----------------------$Title-----------------------
+Read-Host Установка Запуск $EnterPause
+Get-CimInstance -ClassName Win32_OperatingSystem
+Set-ExecutionPolicy unrestricted
+Set-ExecutionPolicy Bypass 
+set-executionpolicy remotesigned
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+Write-Host Установка Завершена!
---
+++ Readme.txt
@@ -0,0 +1,8 @@
+Получение Прав Администратора Запустив PowerShell
+Команды Получения Прав
+1) Get-CimInstance -ClassName Win32_OperatingSystem
+2) Set-ExecutionPolicy unrestricted
+3) Set-ExecutionPolicy Bypass 
+4) set-executionpolicy remotesigned
+5) Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+6) Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
